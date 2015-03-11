a5cd0cccb5604b9130fe73e480d3bd6eed5fdd3b
Marcio Torres <marciojrtorres@gmail.com>
2015-03-11 18:05:44 -0300

    novos codigos

--- intro/src/intro/Main.java
+++ intro/src/intro/Main.java
@@ -1,26 +1,68 @@
-/*
-para compilar: via terminal/prompt
-na pasta do projeto (ex.: intro) digita:
-javac -d bin -cp bin src/intro/Main.java
-
-para executar: java -cp bin pacote.Classe
-por exemplo:
-java -cp bin intro.Main
-
-*/
-
 package intro;
 
 public class Main {
-  // main tab
-  public static void main(String[] args) {
-    // pl tab
-    System.out.println("Estou vivo!");
-
-    int numero = 45;
-
-
-
+  // static: método da classe
+  // int: tipo de retorno
+  // int,int: tipo da entrada (parâmetros, argumentos)
+  static int multiplica(int multiplicando,
+                        int multiplicador) {
+    int resultado = 0;
+    for (int i = 0; i < multiplicador; i++) {
+      resultado += multiplicando;
+    }
+    return resultado;
+  }
+  // 2, 3
+  static int potencia(int base, int exp) {
+    if (exp < 0) {
+      throw new IllegalArgumentException("expoente negativo nao eh permitido");
+    }
+    int resultado = 1;
+    for (int i = 0; i < exp; i++) {
+      resultado = multiplica(base, resultado);
+    }
+    return resultado;
   }
 
+  static int quadrado(int base) {
+    return potencia(base, 2);
+  }
+
+  static int cubo(int base) {
+    return potencia(base, 3);
+  }
+
+  // main tab
+  public static void main(String[] args) {
+    // ASSERTIVA: expressão verdadeira
+    System.out.println(1 == 1);
+    //
+    System.out.println(0.1 == 0.1);
+    System.out.println(0.1 + 0.1 == 0.2);
+    System.out.println(0.1 + 0.1 + 0.1 == 0.3); // false?!?!?!
+    System.out.println(0.1 + 0.1 + 0.1); // 0.30.....4
+    // Floating Point Problem
+
+    System.out.println(multiplica(5, 3) == 15);
+    System.out.println(multiplica(2,12) == 24);
+    // assertiva (caso de teste)
+    // qual é a especificação?
+    // especificação é um contrato
+    // casos de teste felizes
+    System.out.println(potencia(2, 3) == 8);
+    System.out.println(potencia(3, 2) == 9);
+    // casos de teste tristes
+    System.out.println(potencia(3, 1) == 3);
+    System.out.println(potencia(3, 0) == 1);
+    // exceção: caso negado
+    try {
+      System.out.println(potencia(3, -2));
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+
+    System.out.println(quadrado(5) == 25);
+    System.out.println(cubo(5) == 125);
+
+  }
 }
--- stringfu/src/stringfu/Main.java
+++ stringfu/src/stringfu/Main.java
@@ -5,16 +5,39 @@ package stringfu;
 public class Main {
   public static void main(String[] args) {
 
-    String titulo = "tecnico em INFORMATICA para internet";
+    String s = "teste"; // string literal
+    char c = 'a'; // character literal
+    System.out.println((int)(c));
+    // ['t', 'e', 's', 't', 'e']
+    //   0    1    2    3    4
+    char[] chars = s.toCharArray();
+    System.out.println(chars[1]);
+    for (int i = 0; i < chars.length; i++) {
+      System.out.println(chars[i]);
+    }
+    for (int i = 0; i < chars.length; i++) {
+      chars[i] = (char) (chars[i] + 1);
+    }
+    System.out.println(chars);
+    for (int i = 0; i < chars.length; i++) {
+      chars[i] = (char) (chars[i] - 1);
+    }
+    System.out.println(chars);
 
-    // capitalizar
-    // Tecnico em informatica para internet
+    String s1 = "programador ";
+    String s2 = "web";
 
-    // "titulofazer"
-    // Tecnico Em Informatica Para Internet
-
-    // inverter
-    // TECNICO EM informatica PARA INTERNET
+    char[] c1 = s1.toCharArray();
+    char[] c2 = s2.toCharArray();
+    char[] re = new char[c1.length+c2.length];
+    for (int i = 0; i < c1.length; i++) {
+      re[i] = c1[i];
+    }
+    for (int i = c1.length; i < re.length; i++) {
+      re[i] = c2[i - c1.length];
+    }
+    String s3 = new String(re);
+    System.out.println(s3);
 
 
   }
